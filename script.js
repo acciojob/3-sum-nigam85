00c69b48cf70271d03e0f1a8c98ffd26a45e62cb
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-10-09 16:54:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 function threeSum(arr, target) {
   //your code here
-}
+	if (arr.length < 3) {
+    return 0; // Not enough elements to form a triplet
+  }
+		arr.sort((a, b) => a - b); // Sort the array in ascending order
+	  let closestSum = arr[0] + nums[1] + nums[2]; 
 
+	for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+}
+ while (left < right) {
+      const currentSum = arr[i] + arr[left] + arr[right];
+
+      if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
+        closestSum = currentSum; // Update the closest sum
+		  if (currentSum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+  }
+
+  return closestSum;
+      }
 module.exports = threeSum;
